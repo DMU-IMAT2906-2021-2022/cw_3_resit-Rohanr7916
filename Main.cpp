82b03d61729b359a38660c208419b8a441fe1190
Rohan Raja <rohanraja54@gmail.com>
2022-08-11 19:33:31 +0100

    all comments added *done

--- Main.cpp
+++ Main.cpp
@@ -12,16 +12,16 @@
 
 using namespace std;
 
-std::mutex s_Mutex;  
-ProcessTheFile processCSV;  
+std::mutex s_Mutex;  // mutex
+ProcessTheFile processCSV;  // the processing file
 
 
-int m_input = 0;  
+int m_input = 0;  // input a number for (countnumberUp())
 bool b_recursive = true;  
 
 
 
-void outputTime(std::chrono::time_point<std::chrono::system_clock> time_point)
+void outputTime(std::chrono::time_point<std::chrono::system_clock> time_point) //function to output elapsed time
 {
 	while (b_recursive == true)
 	{
@@ -32,9 +32,9 @@ void outputTime(std::chrono::time_point<std::chrono::system_clock> time_point)
 	}
 }
 
-void countNumberUp()  
+void countNumberUp()  // function to count from 0 to the number inpputer
 {
-	std::lock_guard<std::mutex> guard(s_Mutex);  
+	std::lock_guard<std::mutex> guard(s_Mutex); // lock guard 
 	std::cout << "Input an number" << std::endl;
 
 	std::cin >> m_input; 
@@ -53,16 +53,16 @@ int main()
     auto start = std::chrono::system_clock::now();  
 	std::cout << "Coursework 3 Multi Threading" << std::endl;
 
-	std::future<bool> fileSort = std::async(&ProcessTheFile::ProcessFile, &processCSV, "credits.csv", "Credits_OneColumn.csv");
-	std::future<bool> fileProgress = std::async(&ProcessTheFile::progress, &processCSV);  
+	std::future<bool> fileSort = std::async(&ProcessTheFile::ProcessFile, &processCSV, "credits.csv", "Credits_OneColumn.csv"); // async to process csv unto new csvin one coloumn 
+	std::future<bool> fileProgress = std::async(&ProcessTheFile::progress, &processCSV);  //async to give status of file being proccesed 
 
-	std::thread first(countNumberUp);  
-	std::thread second(outputTime, start); 
+	std::thread first(countNumberUp);  // create thread for counting up
+	std::thread second(outputTime, start); // created thread for outpting time
 	
 
 
-	first.join();  
-	second.join();  
+	first.join();  // waits for completed thread
+	second.join(); // waits for completed thrread
 
 	return 0;  
 };
--- ProcessOfFilecpp.cpp
+++ ProcessOfFilecpp.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-std::mutex F_Mutex;  
+std::mutex F_Mutex;  // mutex
 
 ProcessTheFile::ProcessTheFile()   //constructor
 {
@@ -19,18 +19,18 @@ ProcessTheFile::ProcessTheFile()   //constructor
 }
 
 
-bool ProcessTheFile::ProcessFile(std::string m_fileIn, std::string m_fileOut)  
+bool ProcessTheFile::ProcessFile(std::string m_fileIn, std::string m_fileOut)  //function to sort file into 1 column
 {
-	std::lock_guard<std::mutex> guard(F_Mutex);  
+	std::lock_guard<std::mutex> guard(F_Mutex);  //lock guard
 	std::cout << "start" << std::endl;
 
-	ifstream in(m_fileIn);  
-	vector<vector<string>> VecFields;  
+	ifstream in(m_fileIn);  //input file
+	vector<vector<string>> VecFields;  //creates a vector
 
-	ofstream OutputFile;  
+	ofstream OutputFile;   //output file
 	OutputFile.open(m_fileOut, std::ofstream::out | std::ofstream::trunc);  
 
-	if (in) 
+	if (in) //adds the input file to the vector
 	{  
 		string F_Line;
 		while (getline(in, F_Line)) {
@@ -49,7 +49,7 @@ bool ProcessTheFile::ProcessFile(std::string m_fileIn, std::string m_fileOut)
 
 	}
 
-	if (OutputFile.is_open()) 
+	if (OutputFile.is_open()) //output to a csv file
 	{  
 		for (auto row : VecFields) 
 		{
@@ -60,12 +60,12 @@ bool ProcessTheFile::ProcessFile(std::string m_fileIn, std::string m_fileOut)
 		}
 	}
 	
-	F_process = false;  
+	F_process = false;  // process bool to false
 
 	return true;  
 }
 
-bool ProcessTheFile::progress()
+bool ProcessTheFile::progress() // status of file sorting
 {
 	auto start = std::chrono::system_clock::now();
 	while (F_process == true) 
@@ -77,12 +77,12 @@ bool ProcessTheFile::progress()
 		
 
 	}
-	auto end = std::chrono::system_clock::now();
-	std::chrono::duration<double> elapsedTime = end - start;
+	auto end = std::chrono::system_clock::now(); // getting current time
+	std::chrono::duration<double> elapsedTime = end - start; // calculates elapsed time
 	std::lock_guard<std::mutex> guard(F_Mutex);
 
-	std::cout << "Processing complete!" << std::endl;
-	std::cout << "Time take to process file : " << elapsedTime.count() << std::endl;
+	std::cout << "Processing complete!" << std::endl; //output when finished
+	std::cout << "Time take to process file : " << elapsedTime.count() << std::endl; // output elapsed time
 
 	return true;
 }
